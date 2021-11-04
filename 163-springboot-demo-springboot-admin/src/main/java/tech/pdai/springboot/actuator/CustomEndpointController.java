90f0146b1ec97e76eeefa236944c60f716585245
daipeng <peng.dai@siemens.com>
2021-11-04 16:46:52 +0800

    feat: add springboot admin

---
+++ 163-springboot-demo-springboot-admin/src/main/java/tech/pdai/springboot/actuator/CustomEndpointController.java
@@ -0,0 +1,21 @@
+package tech.pdai.springboot.actuator;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pdai
+ */
+@RestController("custom")
+@WebEndpoint(id = "date")
+public class CustomEndpointController {
+
+    @ReadOperation
+    public ResponseEntity<String> currentDate() {
+        return ResponseEntity.ok(LocalDateTime.now().toString());
+    }
+}
---
+++ 163-springboot-demo-springboot-admin/src/main/java/tech/pdai/springboot/actuator/SpringBootHelloWorldApplication.java
@@ -0,0 +1,29 @@
+package tech.pdai.springboot.actuator;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pdai
+ */
+@Configuration
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+    /**
+     * main interface.
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+    }
+
+}
