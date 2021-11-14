aa89029d3c86c317a01a54c3baf7483f1e9f81a7
pdai <suzhou.daipeng@gmail.com>
2021-11-14 16:33:22 +0800

    a

--- 142-springboot-demo-validation-exception-handler/src/main/java/tech/pdai/springboot/validation/eh/exception/GlobalExceptionHandler.java
+++ 142-springboot-demo-validation-exception-handler/src/main/java/tech/pdai/springboot/validation/eh/exception/GlobalExceptionHandler.java
@@ -1,17 +1,19 @@
 package tech.pdai.springboot.validation.eh.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author pdai
@@ -19,6 +21,13 @@ import javax.validation.ValidationException;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    @InitBinder
+    public void handleInitBinder(WebDataBinder dataBinder){
+        dataBinder.registerCustomEditor(Date.class,
+                new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), false));
+    }
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = {BindException.class, ValidationException.class, MethodArgumentNotValidException.class})
