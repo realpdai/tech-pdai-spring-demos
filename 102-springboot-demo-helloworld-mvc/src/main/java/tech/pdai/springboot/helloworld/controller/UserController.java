170eaffd180692ddbef95d5eebf12d7d86125c02
wanghuilong <w_huilong@163.com>
2022-10-06 20:16:18 +0800

    fixbug

--- 102-springboot-demo-helloworld-mvc/src/main/java/tech/pdai/springboot/helloworld/controller/UserController.java
+++ 102-springboot-demo-helloworld-mvc/src/main/java/tech/pdai/springboot/helloworld/controller/UserController.java
@@ -3,9 +3,7 @@ package tech.pdai.springboot.helloworld.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.pdai.springboot.helloworld.entity.User;
 import tech.pdai.springboot.helloworld.service.UserService;
 
@@ -25,8 +23,8 @@ public class UserController {
      * @param user user param
      * @return user
      */
-    @RequestMapping("add")
-    public User add(User user) {
+    @PostMapping("add")
+    public User add(@RequestBody User user) {
         userService.addUser(user);
         return user;
     }
